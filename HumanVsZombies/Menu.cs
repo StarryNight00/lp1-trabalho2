93b739a16da515a7e199addf47a1d790fef870f8
StarryNight00 <cisa.matias@gmail.com>
2019-05-16 15:18:18 +0100

    Completed menu options + methods. Except Start

--- HumanVsZombies/Menu.cs
+++ HumanVsZombies/Menu.cs
@@ -18,44 +18,53 @@ namespace HumanVsZombies
         private void MainMenu() {
             Console.Clear();
             Console.WriteLine("MENU");
-            Console.WriteLine("Start");
-            Console.WriteLine("Quit");
-            Console.WriteLine("Credits");
+            Console.WriteLine("1 - Start");
+            Console.WriteLine("2 - Credits");
+            Console.WriteLine("3 - Quit");
+
+            Console.WriteLine();
 
             switch (Convert.ToInt32(Console.ReadLine()))
             {
                 case 1:
-                    Console.WriteLine("START");
+                    Start();
                     break;
                 case 2:
-                    Console.WriteLine("QUIT");
+                    Credits();
                     break;
                 case 3:
-                    Console.WriteLine("CREDITS");
+                    Quit();
                     break;
                 default:
                     Console.WriteLine("Invalid input.");
                     break;
             }
 
+            Console.ReadLine();
         }
 
+        //Method to start game
         private void Start()
         {
-
+            //Go to GameLoop
         }
 
-        private void Quit()
-        {
-
-        }
-
+        //Method for Credits
         private void Credits()
         {
+            Console.Clear();
             Console.WriteLine("PROJECT BY:");
-            Console.WriteLine("Diana");
-            Console.WriteLine("Sara");
-            Console.WriteLine("Catarina");
+            Console.WriteLine("Diana Noia a21703004");
+            Console.WriteLine("Sara Gama a21705494");
+            Console.WriteLine("Catarina Matias a21801693");
+            Console.ReadKey();
+            MainMenu();
+        }
+
+        //Method to quit game
+        private void Quit()
+        {
+            Environment.Exit(0);
         }
 
     }
