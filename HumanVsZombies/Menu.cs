937de3b8b1d7f4a3109ff996f0b0453177f6f15d
StarryNight00 <cisa.matias@gmail.com>
2019-05-16 14:25:31 +0100

    Class Menu updated with non-functioning methods

--- HumanVsZombies/Menu.cs
+++ HumanVsZombies/Menu.cs
@@ -10,7 +10,53 @@ namespace HumanVsZombies
         public void Intro() {
             Console.Clear();
             Console.WriteLine("Humanos VS Zombies");
+            Console.ReadKey();
+            MainMenu();
+        }
+
+        //Menu
+        private void MainMenu() {
+            Console.Clear();
+            Console.WriteLine("MENU");
+            Console.WriteLine("Start");
+            Console.WriteLine("Quit");
+            Console.WriteLine("Credits");
+
+            switch (Convert.ToInt32(Console.ReadLine()))
+            {
+                case 1:
+                    Console.WriteLine("START");
+                    break;
+                case 2:
+                    Console.WriteLine("QUIT");
+                    break;
+                case 3:
+                    Console.WriteLine("CREDITS");
+                    break;
+                default:
+                    Console.WriteLine("Invalid input.");
+                    break;
+            }
 
         }
+
+        private void Start()
+        {
+
+        }
+
+        private void Quit()
+        {
+
+        }
+
+        private void Credits()
+        {
+            Console.WriteLine("PROJECT BY:");
+            Console.WriteLine("Diana");
+            Console.WriteLine("Sara");
+            Console.WriteLine("Catarina");
+        }
+
     }
 }
