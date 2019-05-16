d8488b221c815f40b088845cda2359953792adbb
StarryNight00 <cisa.matias@gmail.com>
2019-05-16 13:25:02 +0100

    Add project files.

---
+++ HumanVsZombies/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HumanVsZombies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
