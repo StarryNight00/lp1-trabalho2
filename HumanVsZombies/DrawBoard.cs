8d79f27e96c1afd54e30ead037ff910799f0612c
StarryNight00 <cisa.matias@gmail.com>
2019-05-18 15:12:03 +0100

    Board and DrawBoard classes. not yet printing board

---
+++ HumanVsZombies/DrawBoard.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HumanVsZombies
+{
+    class DrawBoard
+    {
+
+        //render the board
+        public void BoardRender(Board board)
+        {
+            //variaveis
+            int column = board.Height;
+            int row = board.Width;
+
+
+            for (int i = 0; i <= row; i++)
+                for (int j = 0; j >= column; j++)
+
+
+        }
+
+        //variaveis
+
+
+
+        //render the board
+
+
+    }
+}
