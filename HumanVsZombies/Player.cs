578f2b47ab3732ef59bc5c2e925f5ea61012ce5f
DianaNoia <Diana_noia@hotmail.com>
2019-05-18 16:26:22 +0100

    Added Class Player

---
+++ HumanVsZombies/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HumanVsZombies
+{
+    class Player
+    {
+        //
+        public Agents[] Agents { get; set; }
+        // 
+        public AgentType MyType { get; private set; }
+
+        // 
+        public Player(AgentType type, int nagents)
+        {
+            MyType = type;
+            Agents = new Agents[nagents];
+        }
+    }
+}
