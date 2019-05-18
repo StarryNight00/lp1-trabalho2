d379bc99e2b2b91ddfe449685465f5d1122fbd9e
DianaNoia <Diana_noia@hotmail.com>
2019-05-18 16:25:36 +0100

    Added Class Agents

---
+++ HumanVsZombies/Agents.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HumanVsZombies
+{
+    class Agents
+    {
+        private Player player;
+
+        public AgentType MyType { get; private set; }
+
+        public Agents(AgentType mytype)
+        {
+            MyType = mytype;
+        }
+    }
+}
